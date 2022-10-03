ac3c07946031990c43aea21cf42264a376d16bc6
Orlando-Z <ozambrana86@gmail.com>
2022-10-03 14:00:16 -0500

    header start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import { Container, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
+import BarberLogoWhite from './app/img/logo_white_blackBackground.png'; 
+ 
 function App() {
   return (
     <div className="App">
-      How To Fade Hair Barber School
+      <Navbar dark  sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={BarberLogoWhite} alt='How to fade hair barber school logo' />
+            <h2>How to fade Hair Barber School</h2>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Body of website here / Info
     </div>
   );
 }
